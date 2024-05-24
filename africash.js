9b8fd01dd40636936b09cbb31b5eebb01bc52396
Lexicon <iyiolad21@gmail.com>
2024-05-25 00:02:04 +0100

    africashredo

---
+++ africash.js
@@ -0,0 +1,5 @@
+
+document.getElementById('hamburger').addEventListener('click', function () {
+    document.getElementById('mobileMenu').classList.toggle('hidden');
+    document.getElementById('menu').classList.toggle('flex');
+});
