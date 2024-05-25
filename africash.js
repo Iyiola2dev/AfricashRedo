7a3951ea180de3621431cdf6937ec8df68157603
Lexicon <iyiolad21@gmail.com>
2024-05-25 18:15:46 +0100

    nm

--- africash.js
+++ africash.js
@@ -3,3 +3,4 @@ document.getElementById('hamburger').addEventListener('click', function () {
     document.getElementById('mobileMenu').classList.toggle('hidden');
     document.getElementById('menu').classList.toggle('flex');
 });
+
